88d0729678c53f322bd3decd9524821f2936faf1
Brandon <brandonmbarrantes@gmail.com>
2025-09-20 18:18:27 -0600

    first commit

---
+++ Tarea/Controllers/MatriculasController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Tarea.Models;
+
+namespace Tarea.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MatriculasController : ControllerBase
+    {
+        //Lista de Matriculas vacia
+        private static List<Matricula> Matriculas;
+
+
+        //constructor por defecto
+        public MatriculasController()
+        {
+            if (Matriculas == null) {
+                Matriculas = new List<Matricula>(); }
+
+        }
+
+        //metodo GET
+        [HttpGet]
+        [Route("getMatricula")]
+        public List<Matricula> Get() 
+        {
+            
+            return Matriculas;   
+        }
+
+        [HttpGet]
+        [Route("getPago")]
+        public string GetPago(int num)
+        {
+            string msj = "No se pudo calcular el total";
+            foreach (var item in Matriculas) {
+                if (item.NumMatricula == num) {
+                    double pagoTotal = item.MontoPago - (item.MontoPago * item.Descuento);
+                    msj = $"El monto total a pagar es {pagoTotal}";
+                }
+            }
+            return msj;
+            
+        }
+
+        [HttpPut]
+        [Route("Add")]
+        public string Add(Matricula matricula) 
+        {
+            string msj = "Error al añadir";
+            try
+            {
+                Matriculas.Add(matricula);
+                msj = "Matrula añadida";
+            } catch(Exception ex)
+            {
+                msj = ex.Message;
+            }
+            return msj;
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public string Delete(int NumMatricula) {
+
+            string msj = "Error al eliminar";
+            try
+            {
+                foreach(var temp in Matriculas)
+                {
+                    if (temp.NumMatricula == NumMatricula)
+                    {
+                        Matriculas.Remove(temp);
+                        msj = "Matricula eliminada";
+                    }
+                }
+            } catch(Exception ex)
+            {
+                msj = ex.Message;
+            }
+
+            return msj;
+        }
+
+        [HttpPost]
+        [Route("Update")]
+        public string update(Matricula mat)
+        {
+            string msj = "Error al actualizar";
+            try
+            {
+                var pMat = Matriculas.FirstOrDefault(r => r.NumMatricula == mat.NumMatricula);
+                if (pMat != null)
+                {
+                    ;
+                    Matriculas.Remove(pMat);
+                    Matriculas.Add(mat);
+                    msj = "Matricula actualizado";
+                }
+            }
+            catch (Exception ex) {
+            
+                msj =ex.Message;
+            }
+
+            return msj;
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Tarea/Models/Matricula.cs
@@ -0,0 +1,13 @@
+﻿namespace Tarea.Models
+{
+    public class Matricula
+    {
+        public int NumMatricula { get; set; }
+        public string Carnet {  get; set; }
+        public string NombreEstudiante { get; set; }
+        public string Curso { get; set; }
+        public DateTime FechaPago { get; set; }
+        public int MontoPago { get; set; }
+        public double Descuento { get; set; }
+    }
+}
